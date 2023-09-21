10ea960a40843422e2420373090506bc5ff8b8dc
sabdulazeem01 <sabdulazeem01@gmail.com>
2023-09-21 23:21:51 +0530

    completed exercise d

--- exercise/d_control_flow_strings/src/main.rs
+++ exercise/d_control_flow_strings/src/main.rs
@@ -20,6 +20,15 @@ fn main() {
         // - If arg is "double", then call the double() function
         // - If arg is anything else, then call the count() function, passing "arg" to it.
 
+        println!("The argument here is {}", arg.to_string());
+        if arg.starts_with("sum") {
+            sum();
+        } else if arg.starts_with("double") {
+            double();
+        } else {
+            count(arg.to_string());
+        }
+
 
         // 1b. Now try passing "sum", "double" and "bananas" to the program by adding your argument
         // after "cargo run".  For example "cargo run sum"
@@ -31,9 +40,12 @@ fn sum() {
     // 2. Use a "for loop" to iterate through integers from 7 to 23 *inclusive* using a range
     // and add them all together (increment the `sum` variable).  Hint: You should get 255
     // Run it with `cargo run sum`
+    for i in 7..=23{
+        sum+=i;
+    }
 
 
-    println!("The sum is {}", sum);
+    println!("The sum is {} \n", sum);
 }
 
 fn double() {
@@ -42,6 +54,11 @@ fn double() {
     // 3. Use a "while loop" to count how many times you can double the value of `x` (multiply `x`
     // by 2) until `x` is larger than 500.  Increment `count` each time through the loop. Run it
     // with `cargo run double`  Hint: The answer is 9 times.
+    while x < 500 {
+        x=2*x;
+        println!("The sum is {} \n", x);
+        count+=1;
+    }
 
 
     println!("You can double x {} times until x is larger than 500", count);
@@ -51,8 +68,14 @@ fn count(arg: String) {
     // Challenge: Use an unconditional loop (`loop`) to print `arg` 8 times, and then break.
     // You will need to count your loops, somehow.  Run it with `cargo run bananas`
     //
-    // print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
-
+    let mut count = 0;
+    loop {
+        if count == 8 {
+            break;
+        }
+        print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
+        count+=1;
+    }   
 
     println!(); // This will output just a newline at the end for cleanliness.
 }
